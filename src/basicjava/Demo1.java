4e25a968559100ac04cb801a2f94d053e40ae1b8
ramprasadsuthi <59646329+ramprasadsuthi@users.noreply.github.com>
2025-05-26 13:59:16 +0530

    Update by Ram

--- src/basicjava/Demo1.java
+++ src/basicjava/Demo1.java
@@ -5,6 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("welcome to mr technologies");
+   System.out.println("Hello this is from Ram");
 	}
 
 }
